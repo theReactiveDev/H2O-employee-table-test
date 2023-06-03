395fc0441b70f11e52cda6d0c2be09bb1dee4a7a
Paveleg99 <paveleg99@mail.ru>
2023-06-04 00:00:20 +0300

    Add sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,10 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
+import cn from "classnames";
+
 import { SidebarItem } from "../../shared/types/sidebar";
+
+import { CompanyLogo } from "../../icons";
 
 import s from "./sidebar.module.scss";
 
@@ -9,12 +13,22 @@ interface SidebarProps {
 }
 
 const Sidebar: FC<SidebarProps> = ({ links }) => {
+  const [activeLink, setActiveLink] = useState<number | null>(null);
   return (
     <nav className={s.sidebar}>
       <div className={s.container}>
+        <div className={s.logo}>
+          <Link to="/">
+            <CompanyLogo />
+          </Link>
+        </div>
         <ul className={s.links}>
           {links.map((item) => (
-            <li key={item.index}>
+            <li
+              key={item.index}
+              className={cn(s.link, { [s.active]: activeLink === item.index })}
+              onClick={() => setActiveLink(item.index)}
+            >
               <Link to={item.link}>{item.image}</Link>
               {/* {item.image} */}
             </li>
